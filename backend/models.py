2df50b2d17eaeaa2c3de3661c2dae701233d1da6
Nathiya Sundharam <nasundharam@fssa.freshworks.com>
2026-06-02 08:36:38 +0530

    change folder1

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Transaction(BaseModel):
-    pan: str
-    inv_name: str
-    scheme: str
-    traddate: str  # Expected format YYYY-MM-DD
-    amount: float
-    units: float
